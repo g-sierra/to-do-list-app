b368cbb67bf82e47b28697e7cc22752d0d74aa3a
g-sierra <193622299+g-sierra@users.noreply.github.com>
2025-10-03 23:05:17 -0300

    fix: prevent null objects from being added to the tasks array
    this was causing the rendering to break if the input was empty when
    triggering the addTask command

--- js/main.js
+++ js/main.js
@@ -2,14 +2,15 @@ import { d, taskInput, addBtn } from "./dom.js";
 import { getTaskFromInput, addTask } from "./tasks.js";
 import renderTaskList from "./render.js";
 
+const handleAdd = () => {
+  const newTask = getTaskFromInput(taskInput);
+  if (newTask) addTask(newTask);
+};
+
 const main = () => {
-  addBtn.addEventListener("click", () => {
-    addTask(getTaskFromInput(taskInput));
-  });
+  addBtn.addEventListener("click", handleAdd);
   taskInput.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-      addTask(getTaskFromInput(taskInput));
-    }
+    if (e.key === "Enter") handleAdd();
   });
 
   renderTaskList();
